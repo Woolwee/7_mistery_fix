58f9a9cc90282c32440df0513990099cf774a421
Woolwee <grdmitriyl@gmail.com>
2017-08-09 08:53:01 +0300

    The script has been repaired

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,11 +1,19 @@
 from math import sqrt
+#from cmath import sqrt as csqrt  # I thought i should write a solution for
+                                  # for complex cases
 
 
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
-    root1 = (-b - sqrt(discriminant)) / (2 * a)
-    root2 = (-b + sqrt(discriminant)) / (2 * a)
-    if discriminant == 0:
-        return root1, None
+    if discriminant < 0:
+        #root1 = (-b - csqrt(discriminant)) / (2 * a)
+        #root2 = (-b + csqrt(discriminant)) / (2 * a)
+        #return root1, root2
+        return None, None
     else:
-        return root1, root2
+        root1 = (-b - sqrt(discriminant)) / (2 * a)
+        root2 = (-b + sqrt(discriminant)) / (2 * a)
+        if discriminant == 0:
+            return root1, None
+        else:
+            return root1, root2
