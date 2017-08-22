2b3399094cd1004a7bb5cd9bde3ecebe0c6a0199
Woolwee <grdmitriyl@gmail.com>
2017-08-22 13:37:25 +0300

    reduced the nesting in get_roots

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,14 +1,13 @@
-from math import sqrt
+from cmath import sqrt
 
 
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
+    root1 = (-b - sqrt(discriminant).real) / (2 * a)
+    root2 = (-b + sqrt(discriminant).real) / (2 * a)
     if discriminant < 0:
         return None, None
+    elif discriminant == 0:
+        return root1, None
     else:
-        root1 = (-b - sqrt(discriminant)) / (2 * a)
-        if discriminant == 0:
-            return root1, None
-        else:
-            root2 = (-b + sqrt(discriminant)) / (2 * a)
-            return root1, root2
+        return root1, root2
